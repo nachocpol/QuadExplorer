20249b775aad7a1854205c30640c64bb52416462
Nacho Cortizo <nachocpol@gmail.com>
2020-04-28 19:58:37 +0100

    Updated ignore. Board update. Readme.md update.

--- Board/src/main.cpp
+++ Board/src/main.cpp
@@ -1,11 +1,52 @@
 #include <Arduino.h>
+#include <ArduinoBLE.h>
 
 #include "QuadFlyController.h"
 
-void setup() {
-  // put your setup code here, to run once:
+BLEService g_TestService("180F");
+
+QuadFlyController FC;
+FCQuadState State;
+
+float totalTime = 0.0f; // in ms
+float deltaTime = 0.0f; // in ms
+
+const int k_PinMotor1 = 5;
+const int k_PinMotor2 = 4;
+const int k_PinMotor3 = 3;
+const int k_PinMotor4 = 2;
+
+void setup() 
+{
+  delay(5000);
+
+  analogWrite(k_PinMotor1, 5);
+  analogWrite(k_PinMotor2, 5);
+  analogWrite(k_PinMotor3, 5);
+  analogWrite(k_PinMotor4, 5);
+
+  delay(10000);
+
+  analogWrite(k_PinMotor1, 0);
+  analogWrite(k_PinMotor2, 0);
+  analogWrite(k_PinMotor3, 0);
+  analogWrite(k_PinMotor4, 0);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  unsigned long startTime = millis();
+
+  State.DeltaTime = deltaTime;
+  State.Time = totalTime;
+
+  State.Height = 0.0f;
+  State.Pitch = 0.0f;
+  State.Yaw = 0.0f;
+  State.Roll = 0.0f;
+
+  FCCommands commands = FC.Iterate(State);
+
+  deltaTime = (float)startTime - (float)millis();
+  totalTime += deltaTime;
 }
--- Readme.md
+++ Readme.md
@@ -1,3 +1,12 @@
-Quad Explorer
+# Quad Explorer
 
-Quadcopter flight simulation and flight controller implementation.
+This is my attempt to create a standalone quad copter, from the hardware to the software that makes it fly. This project is divided in several parts:
+
+## QuadExplorerApp
+This app simulates the drone flying. It uses my own 3D rendering framework and PhysicsX to simulate the behaviour of the quad. This software is useful to test the flight controller without deploying it to the board. It can also be used to  tweak the gains for the different PID controllers.
+
+## Board
+Software that runs of the quadcopter hardware. This implements basic things like sensor reading, noise removal and BT/Serial conections.
+
+## ControllerApp
+Unity project to control the drone from a phone. To build this from source, you need the "Bluetooth LE for iOS, tvOS and Android" library.
